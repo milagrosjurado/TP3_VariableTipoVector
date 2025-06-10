03c39e4ae22a4448f5ac703aae60eb38bdbb6ebc
Jurado Milagros <Milagrosjurado2008@gmail.com>
2025-06-10 10:20:47 -0300

    Add files via upload

---
+++ TP3-Ejercicio3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Ingrese la cantidad de numeros: ";
+    cin >> n;
+
+    int numeros[n];
+
+    cout << "Ingrese los " <<n<< " numeros separados por un espacio: " <<endl;
+   for (int i=0; i<n; i++){
+   	cin >> numeros[i];
+   }
+
+    for (int i=0; i<n-1; i++) {
+        for (int j=0; j<n-i-1; j++) {
+            if (numeros[j] > numeros[j+1]) {
+                int num = numeros[j];
+                numeros[j] = numeros[j+1];
+                numeros[j+1] = num;
+            }
+        }
+    }
+
+    cout << "Numeros ordenados: "<<endl;
+    for (int i=0; i<n; i++) {
+        cout << numeros[i] << " ";
+    }
+
+    return 0;
+}
